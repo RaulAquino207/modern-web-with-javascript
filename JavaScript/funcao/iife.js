e080390c143ee0d3c97fe5a7dd238a32eb543ea1
Raul Aquino <aquinoraul207@gmail.com>
2019-03-07 15:30:31 -0300

    Remove JavaScript folder

--- JavaScript/funcao/iife.js
+++
@@ -1,5 +0,0 @@
-//IIFE -> Immediately invoked function expression
-(function(){
-    console.log('Será executado na hora!')
-    console.log('Foge do escopo mais abrangente!')
-})()
--- JavaScript/fundamentos/opEstranhas.js
+++
@@ -1,3 +0,0 @@
-console.log(7/0)
-//pelo fato de JS ser fracamento tipada ela consegue fazer essa divisão entre uma string e um nº inteiro
-console.log('10'/2)
