b27632a58b9d1703ab46e7c61d314e9ffc2fa237
Raul Aquino <aquinoraul207@gmail.com>
2019-03-20 00:50:58 -0300

    made changes in oo.js

--- JavaScript-Web-course/orientacaoObjeto/oo.js
+++ JavaScript-Web-course/orientacaoObjeto/oo.js
@@ -2,3 +2,21 @@
 
 //procedural
 processamento (valor1 , valor2, valor3)
+
+//Agora em OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() //Foco passou a ser o objeto
+
+//Principios importantes:
+//1. abstração
+//2. 
+
+
