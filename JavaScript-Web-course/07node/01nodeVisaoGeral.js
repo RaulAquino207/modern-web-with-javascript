027e743568f3847c178f00096f519b0218925445
Raul Aquino <aquinoraul207@gmail.com>
2019-10-21 17:17:42 -0300

    started studies on NODE JS

---
+++ JavaScript-Web-course/07node/01nodeVisaoGeral.js
@@ -0,0 +1,5 @@
+/*
+Node é extremamente relevante no mundo do JS, trouxe uma revolução no JS/WEB. Muitas empresas 
+usam NODE como solução para o backend
+NODE - Ambiente de execução de JS do lado servidor(não esta rodando dentro do browser)
+*/
